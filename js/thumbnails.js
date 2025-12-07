f74ebc3f68c65a6802948b25b023458c124be016
Виктория Мандрыченко <rdanilov405@gmail.com>
2025-12-07 17:45:17 +0500

    Доработала модуль миниатюр — добавила обработчик клика

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,3 +1,5 @@
+import { openBigPicture } from './big-picture.js';
+
 const picturesContainer = document.querySelector('.pictures');
 const pictureTemplate = document
   .querySelector('#picture')
@@ -7,18 +9,23 @@ const pictureTemplate = document
 export const renderThumbnails = (data) => {
   const fragment = document.createDocumentFragment();
 
-  data.forEach(({ url, description, likes, comments }) => {
+  data.forEach((picture) => {
     const pictureElement = pictureTemplate.cloneNode(true);
 
     const img = pictureElement.querySelector('.picture__img');
-    img.src = url;
-    img.alt = description;
+    img.src = picture.url;
+    img.alt = picture.description;
 
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments;
+    pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+    pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+
+    pictureElement.addEventListener('click', () => {
+      openBigPicture(picture);
+    });
 
     fragment.appendChild(pictureElement);
   });
 
   picturesContainer.appendChild(fragment);
 };
+
